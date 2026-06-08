9ff2b077843ddac9c191b5bed0779a4276b41583
Mohammed Shadman <mohammedshadman101@gmail.com>
2026-06-09 00:23:21 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,6 +3,7 @@ from . import db
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    institution = db.Column(db.String(100))
     bmi = db.Column(db.Float)
+    last_update = db.Column(db.DateTime, default=db.func.current_timestamp())
+    # This allows you to store the history of generated plans
     diet_plan = db.Column(db.Text)
